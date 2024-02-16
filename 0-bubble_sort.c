b45edf93efe46bc3a218fb04c87d91789a83924e
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-16 16:10:34 +0000

    initialization and task 0: bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+/**
+ * bubble_sort - a bubble sort function for arrays
+ *@array: the array to be sorted
+ *@size: the size of the array
+ *Return: the sorted array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int sorted = 0;
+	size_t n = 0;
+
+	while (sorted == 0)
+	{
+		for (n = 0; n < size - 1; n++)
+		{
+			if (array[n] > array[n + 1])
+			{
+				swap(array, n, n+1);
+				print_array(array, size);
+			}
+		}
+		
+		sorted = 1;
+
+		for (n = 0; n < size - 1; n++)
+		{
+			if (array[n] > array[n + 1])
+				sorted = 0;
+		}
+	}
+}
+
+/**
+ * swap - exchange the positions of 2 elemnts of an array
+ *@array: the array to be sorted
+ *@pos1: the first position
+ *@pos2: the 2nd position
+ *Return: the sorted array
+ */
+void swap(int *array, int pos1, int pos2)
+{
+	int temp = array[pos1];
+
+	array[pos1] = array[pos2];
+	array[pos2] = temp;
+
+}
